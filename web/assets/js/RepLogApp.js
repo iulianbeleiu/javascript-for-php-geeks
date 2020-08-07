12333b7f4d99a2c85e6019dfce8d30402ae9e48b
Iulian Beleiu <i.beleiu@plan-net.com>
2020-08-07 11:01:06 +0300

    full javascript rendering and js routing

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,9 +1,11 @@
 'use strict';
 
-(function(window, $) {
+(function(window, $, Routing) {
     window.RepLogApp = function($wrapper) {
         this.$wrapper = $wrapper;
         this.helper = new Helper($wrapper);
+
+        this.loadRepLogs();
 
         this.$wrapper.on(
             'click',
@@ -27,6 +29,18 @@
     $.extend(window.RepLogApp.prototype, {
         _selectors: {
             newRepForm: '.js-new-rep-log-form'
+        },
+
+        loadRepLogs: function() {
+            var self = this;
+            $.ajax({
+                url: Routing.generate('rep_log_list'),
+                success: function(data) {
+                    $.each(data.items, function(key, repLog) {
+                        self._addRow(repLog);
+                    });
+                }
+            });
         },
 
         updateTotalWeightLifted: function() {
@@ -151,4 +165,4 @@
             return totalWeight;
         }
     });
-})(window, jQuery);
+})(window, jQuery, Routing);
